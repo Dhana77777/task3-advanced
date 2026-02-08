05a49432ff676f8c7e2c3c062e3344c16bbaa719
Dhana77777 <peddintidhanalaxmi7@gmail.com>
2026-02-08 22:27:12 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include 'db.php';
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row && password_verify($password, $row['password'])) {
+        $_SESSION['user'] = $username;
+        header("Location: dashboard.php");
+    } else {
+        echo "Login failed";
+    }
+}
+?>
+
+<form method="post">
+<input type="text" name="username" placeholder="Username" required>
+<input type="password" name="password" placeholder="Password" required>
+<button name="login">Login</button>
+</form>
